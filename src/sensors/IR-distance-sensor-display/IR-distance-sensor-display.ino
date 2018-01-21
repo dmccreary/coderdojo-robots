10eadb9d866c10de19133d8d0056f8bad61c845b
Dan McCreary <dan.mccreary@gmail.com>
2018-01-21 13:06:36 -0600

    Added display demo for IR sensor

--- src/sensors/IR-distance-sensor-display/IR-distance-sensor-display.ino
+++ src/sensors/IR-distance-sensor-display/IR-distance-sensor-display.ino
@@ -1,10 +1,15 @@
+
 // IR distance sensor using Sharp GP2Y0A21YK0F Analog Distance Sensor
 // The sensor is hooked to A0 and returns a votage reading inversely proportional to the distance to an object
 // Note that readings under 10CM and over 70CM are mostly invalid
 // https://www.ebay.com/itm/GP2Y0A21YK0F-Sharp-IR-Analog-Distance-Sensor-Distance-10-80CM-Free-Cable/202121681621?ssPageName=STRK%3AMEBIDX%3AIT&_trksid=p2057872.m2749.l2649
-
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_PCD8544.h>
 
 #define ir_sensor_pin A0
+
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
 
 int sensor_value;
 // pick typical mid sensor range values
@@ -14,8 +19,13 @@ int dist_scaled;
 int dist_cm;
 int readpin=A0;
 void setup() {
-    pinMode(ir_sensor_pin,INPUT);
-    Serial.begin(9600);
+  display.begin();
+  display.clearDisplay();
+  display.setContrast(45); // change from 40 to 60...
+  display.setTextSize(1); // a resonable size for Nokia 5110 display
+  display.setTextColor(BLACK);
+  pinMode(ir_sensor_pin,INPUT);    
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -38,14 +48,30 @@ void loop() {
     Serial.print(dist_scaled);
     Serial.print(" dist_cm=");
     Serial.println(dist_cm);
+    display_stats();
 }
 
 int average_n_sensor_values(int n) {
-int total=0;
-  for (int i=0; i<n; i++) {
+   int total=0;
+   for (int i=0; i<n; i++) {
     total += analogRead(ir_sensor_pin);
     delay(10);
   }
   return (int) total / n;
 }
+
+void display_stats() {
+  display.clearDisplay();
+  display.println("A0: ");
+  display.setCursor(20,0);
+  display.println(sensor_value);
+  display.print("min: ");
+  display.println(min_sensor_value);
+  display.print("max: ");
+  display.println(max_sensor_value);
+  display.print("percent: ");
+  display.println(dist_scaled);
+  display.display();
+}
+
 
