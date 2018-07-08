d0dcce2d96e45979c57e8a659343315e06d24070
Dan McCreary <dan.mccreary@gmail.com>
2018-07-08 14:29:13 -0500

    working three ping face bot - lack of memory means no graph face

--- src/face-bot/_09-three-ping-display-test/_09-three-ping-display-test.ino
+++ src/face-bot/_09-three-ping-display-test/_09-three-ping-display-test.ino
@@ -11,6 +11,7 @@
 #define RIGHT_TRIGGER_PIN   A4
 #define RIGHT_ECHO_PIN      A5
 #define MAX_DISTANCE 100
+#define TURN_DISTANCE_THRESHOLD 12
 
 // If using software SPI (the default case):
 #define OLED_CLK   13   // SCL
@@ -18,6 +19,7 @@
 #define OLED_RESET 11   // RST
 #define OLED_DC    10   // DC
 #define OLED_CS    8    //CS
+#define LINE_HIGHT  10   // 10 pixels per line
 
 NewPing sonar_left(LEFT_TRIGGER_PIN, LEFT_ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar_center(CENTER_TRIGGER_PIN, CENTER_ECHO_PIN, MAX_DISTANCE);
@@ -28,12 +30,17 @@ Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 int left_dist = 0;
 int center_dist =  0;
 int right_dist =  0;
-int turn_distance_threshold = 12;
+
 
 void setup() {
   display.begin(SSD1306_SWITCHCAPVCC);
+  display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.print("9 3-ping display tst");
+  display.display();
+  delay(3000);
   Serial.begin(9600);
 }
  
@@ -52,46 +59,45 @@ void loop() {
   // erase everything
   display.clearDisplay();
   // we have 64 pixels to work with
-  display.setCursor(0,0);
+  display.setCursor(0,LINE_HIGHT);
   display.print("L:");
-  display.setCursor(10,0);
+  display.setCursor(10,LINE_HIGHT);
   display.print(left_dist);
 
-  display.setCursor(50,0);
+  display.setCursor(50,LINE_HIGHT);
   display.print("M:");
-  display.setCursor(60,0);
+  display.setCursor(60,LINE_HIGHT);
   display.print(center_dist);
 
-  display.setCursor(100,0);
+  display.setCursor(100,LINE_HIGHT);
   display.print("R:");
-  display.setCursor(110,0);
+  display.setCursor(110,LINE_HIGHT);
   display.print(right_dist);
 
     
-  if (right_dist < turn_distance_threshold && right_dist > 0) {
-    display.setCursor(0,30);
+  if (right_dist < TURN_DISTANCE_THRESHOLD && right_dist > 0) {
+    display.setCursor(0,3*LINE_HIGHT);
     display.print("Turning left");
   }
-    else if (left_dist < turn_distance_threshold && left_dist > 0) {
-      display.setCursor(0,30);
+    else if (left_dist < TURN_DISTANCE_THRESHOLD && left_dist > 0) {
+      display.setCursor(0,3*LINE_HIGHT);
       display.print("Turning right");
     }
-    else if (center_dist < turn_distance_threshold && center_dist > 0) {
-      display.setCursor(0,30);
+    else if (center_dist < TURN_DISTANCE_THRESHOLD && center_dist > 0) {
+      display.setCursor(0,3*LINE_HIGHT);
       display.print("Turn center - backing up");
       // measure again
       left_dist = sonar_left.ping_cm();
       right_dist =  sonar_right.ping_cm();
       // if we have more room on the right, turn right
-      display.setCursor(0,40);
+      display.setCursor(0,4*LINE_HIGHT);
       if (right_dist > left_dist) {  
-        display.print("Now turn right");
+        display.print("Next turn right");
       }
         else {
-          display.print("Now turn left");
+          display.print("Next turn left");
         }
     }
-   
   display.display();
 }
 
